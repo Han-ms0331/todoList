2ab06147d7b28657cc13fa84e57faa9d77b4d7ec
Han-ms0331 <76736930+Han-ms0331@users.noreply.github.com>
2021-01-05 03:54:45 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const weather = document.querySelector(".js-weather");
 
 
 function getWeather(lat, lng){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     ).then(function (response){
             return response.json();
     }).then(function(json){
